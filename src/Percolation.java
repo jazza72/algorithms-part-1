38c17acd387df6598c28f7285ac7ebb9bb0d384a
James Wilson <jazzaman.uk@gmail.com>
2017-12-17 21:03:09 +0000

    add eclipse project/classpath files

--- src/Percolation.java
+++ src/Percolation.java
@@ -5,159 +5,162 @@ import edu.princeton.cs.algs4.*;
  *
  */
 public class Percolation {
-	private boolean[][] grid;
-	private int size, opensites;
-	private WeightedQuickUnionUF uf;
-	private int top, bottom;
+  private boolean[][] grid;
+  private int size, opensites;
+  private WeightedQuickUnionUF uf;
+  private int top, bottom;
 
-	public Percolation(int n) {
+  public Percolation(int n) {
 
-		if (n <= 0)
-			throw new IllegalArgumentException();
+    if (n <= 0)
+      throw new IllegalArgumentException();
 
-		size = n;
+    size = n;
 
-		// add 2 extra on here as our virtual top and bottom site, and 1 more as we
-		// don't start indexing the array at 0
-		uf = new WeightedQuickUnionUF(size * size + 2);
+    // add 2 extra on here as our virtual top and bottom site, and 1 more as we
+    // don't start indexing the array at 0
+    uf = new WeightedQuickUnionUF(size * size + 2);
 
-		// create n-by-n grid, with all sites blocked
-		// no need to initialize as boolean is intialized to false by default
-		grid = new boolean[n + 1][n + 1];
+    // create n-by-n grid, with all sites blocked
+    // no need to initialize as boolean is intialized to false by default
+    grid = new boolean[n + 1][n + 1];
 
-		// for (int x=0; x<n; x++)
-		// {
-		// for (int y; y<n; y++)
-		// {
-		// grid[x][y] = 0;
-		// }
-		// }
+    // for (int x=0; x<n; x++)
+    // {
+    // for (int y; y<n; y++)
+    // {
+    // grid[x][y] = 0;
+    // }
+    // }
 
-		// top and bottom indices
-		top = size * size;
-		bottom = size * size + 1;
-	}
+    // top and bottom indices
+    top = size * size;
+    bottom = size * size + 1;
+  }
 
-	// open site (row, col) if it is not open already
-	public void open(int row, int col) {
-		validate(row, col);
+  // open site (row, col) if it is not open already
+  public void open(int row, int col) {
+    validate(row, col);
 
-		if (grid[row][col] == false) {
-			grid[row][col] = true;
-		} else {
-			return;
-		}
+    if (grid[row][col] == false) {
+      grid[row][col] = true;
+    } else {
+      return;
+    }
 
-		opensites++;
+    opensites++;
 
-		// now connect the open cell to the surrounding ones
+    // now connect the open cell to the surrounding ones
 
-		// if this is a top row site being opened, then connect it to the virtual top
-		// site
-		if (row == 1) {
-			uf.union(top, xyToID(row, col));
-		}
+    // if this is a top row site being opened, then connect it to the virtual top
+    // site
+    if (row == 1) {
+      uf.union(top, xyToID(row, col));
+    }
 
-		if (row > 1) {
-			if (isOpen(row - 1, col)) {
-				uf.union(xyToID(row, col), xyToID(row - 1, col));
-			}
-		}
+    if (row > 1) {
+      if (isOpen(row - 1, col)) {
+        uf.union(xyToID(row, col), xyToID(row - 1, col));
+      }
+    }
 
-		if (col > 1) {
-			if (isOpen(row, col - 1)) {
-				uf.union(xyToID(row, col), xyToID(row, col - 1));
-			}
-		}
+    if (col > 1) {
+      if (isOpen(row, col - 1)) {
+        uf.union(xyToID(row, col), xyToID(row, col - 1));
+      }
+    }
 
-		if (col < size) {
-			if (isOpen(row, col + 1)) {
-				uf.union(xyToID(row, col), xyToID(row, col + 1));
-			}
-		}
+    if (col < size) {
+      if (isOpen(row, col + 1)) {
+        uf.union(xyToID(row, col), xyToID(row, col + 1));
+      }
+    }
 
-		if (row < size) {
-			if (isOpen(row + 1, col)) {
-				uf.union(xyToID(row, col), xyToID(row + 1, col));
-			}
-		}
+    if (row < size) {
+      if (isOpen(row + 1, col)) {
+        uf.union(xyToID(row, col), xyToID(row + 1, col));
+      }
+    }
 
-		// connect bottom row to virtual bottom site
-		if (row == size) {
-			uf.union(bottom, xyToID(row, col));
-		}
-	}
+    // connect bottom row to virtual bottom site
+    if (row == size) {
+      uf.union(bottom, xyToID(row, col));
+    }
+  }
 
-	public boolean isOpen(int row, int col) {
-		validate(row, col);
-		// is site (row, col) open?
-		return grid[row][col];
-	}
+  public boolean isOpen(int row, int col) {
+    validate(row, col);
+    // is site (row, col) open?
+    return grid[row][col];
+  }
 
-	public boolean isFull(int row, int col) {
-		validate(row, col);
-		if (isOpen(row, col)) {
-			return (uf.connected(top, xyToID(row, col)));
-		}
-		
-		return false;
-	}
-		
+  public boolean isFull(int row, int col) {
+    validate(row, col);
+    if (isOpen(row, col)) {
+      return (uf.connected(top, xyToID(row, col)));
+    }
 
-	public int numberOfOpenSites() {
-		return opensites; // number of open sites
-	}
+    return false;
+  }
 
-	/**
-	 * This method should simply check if there is a connection between the virtual
-	 * top and bottom sites
-	 * 
-	 * @return
-	 */
-	public boolean percolates() {
-		return uf.connected(top, bottom);
-	}
+  public int numberOfOpenSites() {
+    return opensites; // number of open sites
+  }
 
-	/**
-	 * Method to covert 2D grid position to 1D arrary position in union-find object
-	 * 
-	 * @param x
-	 * @param y
-	 * @return array index for union-find
-	 */
-	private int xyToID(int x, int y) {
-		// we always start the grid ref from 1 (not 0)
-		// uf array position is calculated from x, y coord values
+  /**
+   * This method should simply check if there is a connection between the virtual top and bottom
+   * sites
+   * 
+   * @return
+   */
+  public boolean percolates() {
+    return uf.connected(top, bottom);
+  }
 
-		return (y + (x - 1) * size)-1;
+  /**
+   * Method to covert 2D grid position to 1D arrary position in union-find object
+   * 
+   * @param x
+   * @param y
+   * @return array index for union-find
+   */
+  private int xyToID(int x, int y) {
+    // we always start the grid ref from 1 (not 0)
+    // uf array position is calculated from x, y coord values
 
-	}
+    return (y + (x - 1) * size) - 1;
 
-	private boolean validate(int x, int y) {
+  }
 
-		if (x < 1 || x > size || y < 1 || y > size)
-			throw new IllegalArgumentException();
+  private boolean validate(int x, int y) {
 
-		return true;
-	}
+    if (x < 1 || x > size || y < 1 || y > size)
+      throw new IllegalArgumentException();
 
-	public static void main(String[] args) {
+    return true;
+  }
 
-		Percolation p = new Percolation(5);
+  public static void main(String[] args) {
 
-		p.open(1, 1);
-		p.open(2, 1);
-		p.open(3, 1);
-		p.open(4, 1);
-		p.open(5, 1);
-		p.open(3, 4);
+    // Percolation p = new Percolation(5);
+    //
+    // p.open(1, 1);
+    // p.open(2, 1);
+    // p.open(3, 1);
+    // p.open(4, 1);
+    // p.open(5, 1);
+    // p.open(3, 4);
+    //
+    // if (p.percolates()) {
+    // System.out.println("true");
+    // } else {
+    // System.out.println("false");
+    // }
 
-		if (p.percolates()) {
-			System.out.println("true");
-		} else {
-			System.out.println("false");
-		}
+    double result = Math.pow(2, 30);
 
-		// test client (optional)
-	}
+    StdOut.println(result);
+
+    // test client (optional)
+  }
 }
--- src/PercolationStats.java
+++ src/PercolationStats.java
@@ -9,75 +9,76 @@ import edu.princeton.cs.algs4.StdStats;
  */
 public class PercolationStats {
 
-	private double[] data;
-	private int t, size;
-	private double mean;
-	private double stddev;
+  private double[] data;
+  private int t, size;
+  private double mean;
+  private double stddev;
 
-	public PercolationStats(int n, int trials) {
-		// perform trials independent experiments on an n-by-n grid
-		if (n <= 0 || trials <= 0)
-			throw new IllegalArgumentException();
-		
-		size = n;
-		t = trials;
-		data = new double[trials];
+  public PercolationStats(int n, int trials) {
+    // perform trials independent experiments on an n-by-n grid
+    if (n <= 0 || trials <= 0)
+      throw new IllegalArgumentException();
 
-		for (int i = 0; i < trials; i++) {
-			data[i] = runTests();
-		}
-	}
+    size = n;
+    t = trials;
+    data = new double[trials];
 
-	private double runTests() {
+    for (int i = 0; i < trials; i++) {
+      data[i] = runTests();
+    }
+  }
 
-		Percolation perc = new Percolation(size);
+  private double runTests() {
 
-		while (!perc.percolates()) {
-			int x = StdRandom.uniform(1, size+1);
-			int y = StdRandom.uniform(1, size+1);
+    Percolation perc = new Percolation(size);
 
-			if (!perc.isOpen(x, y)) {
-				perc.open(x, y);
-			}
-		}
+    while (!perc.percolates()) {
+      int x = StdRandom.uniform(1, size + 1);
+      int y = StdRandom.uniform(1, size + 1);
 
-		// return the p* threshold value
-		return perc.numberOfOpenSites() / Math.pow(size, 2);
+      if (!perc.isOpen(x, y)) {
+        perc.open(x, y);
+      }
+    }
 
-	}
+    // return the p* threshold value
+    return perc.numberOfOpenSites() / Math.pow(size, 2);
 
-	public double mean() {
-		// sample mean of percolation threshold
-		this.mean = StdStats.mean(data);
-		return this.mean;
-	}
+  }
 
-	public double stddev() {
-		// sample standard deviation of percolation threshold
-		this.stddev = StdStats.stddev(data);
-		return this.stddev;
-	}
+  public double mean() {
+    // sample mean of percolation threshold
+    this.mean = StdStats.mean(data);
+    return this.mean;
+  }
 
-	public double confidenceLo() {
-		// low endpoint of 95% confidence interval
-		return mean() - ((1.96*stddev())/Math.sqrt(t));
-	}
-	
-	public double confidenceHi() {
-		// high endpoint of 95% confidence interval
-		return mean() + ((1.96*stddev())/Math.sqrt(t));
-	}
+  public double stddev() {
+    // sample standard deviation of percolation threshold
+    this.stddev = StdStats.stddev(data);
+    return this.stddev;
+  }
 
-	public static void main(String[] args) {
-		// test client (described below)
-		int n = Integer.parseInt(args[0]);
-		int t = Integer.parseInt(args[1]);
+  public double confidenceLo() {
+    // low endpoint of 95% confidence interval
+    return mean() - ((1.96 * stddev()) / Math.sqrt(t));
+  }
 
-		PercolationStats ps = new PercolationStats(n, t);
-		
-		StdOut.println("mean\t\t\t= " + ps.mean());
-		StdOut.println("stddev\t\t\t= " + ps.stddev());
-		StdOut.println("95% confidence interval\t= [" + ps.confidenceLo() + "," + ps.confidenceHi() + "]");
+  public double confidenceHi() {
+    // high endpoint of 95% confidence interval
+    return mean() + ((1.96 * stddev()) / Math.sqrt(t));
+  }
 
-	}
+  public static void main(String[] args) {
+    // test client (described below)
+    int n = Integer.parseInt(args[0]);
+    int t = Integer.parseInt(args[1]);
+
+    PercolationStats ps = new PercolationStats(n, t);
+
+    StdOut.println("mean\t\t\t= " + ps.mean());
+    StdOut.println("stddev\t\t\t= " + ps.stddev());
+    StdOut.println(
+        "95% confidence interval\t= [" + ps.confidenceLo() + "," + ps.confidenceHi() + "]");
+
+  }
 }
